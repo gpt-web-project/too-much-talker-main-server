b8e1a401e8a5c707aa41c6f917381bfb75dc1a36
gayeonwon <henzel1013@gmail.com>
2024-01-15 16:22:56 +0900

    환경설정

---
+++ src/main/java/org/sideproject/MainApplication.java
@@ -0,0 +1,13 @@
+package org.sideproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
